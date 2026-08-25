a4d284f5eac0a86a61e5b1b82f0f22b1858431b5
ytalo356476 <ninhar339@gmail.com>
2026-08-24 21:09:05 -0300

    Update FuncionarioCLT.java

--- Exercicio2/src/main/java/FuncionarioCLT.java
+++ Exercicio2/src/main/java/FuncionarioCLT.java
@@ -1,2 +1,31 @@
-public class FuncionarioCLT {
+public class FuncionarioCLT extends Funcionario {
+
+    private double valeTransporte;
+    private double valeAlimentacao;
+
+    public FuncionarioCLT(
+            String nome,
+            String matricula,
+            double salarioBase,
+            String dataAdmissao,
+            double valeTransporte,
+            double valeAlimentacao) {
+
+        super(nome, matricula, salarioBase, dataAdmissao);
+
+        this.valeTransporte = valeTransporte;
+        this.valeAlimentacao = valeAlimentacao;
+    }
+
+    @Override
+    public double calcularSalario() {
+        return super.calcularSalario()
+                + valeTransporte
+                + valeAlimentacao;
+    }
+
+    @Override
+    public double calcularDesconto() {
+        return super.calcularDesconto() + 50.00;
+    }
 }

c91b84f5b6a26fee81d025c1a11a54c83029eb9a
ytalo356476 <ninhar339@gmail.com>
2026-08-24 21:07:57 -0300

    Update Funcionario.java

--- Exercicio2/src/main/java/Funcionario.java
+++ Exercicio2/src/main/java/Funcionario.java
@@ -1,2 +1,46 @@
-public class Funcionario {
+public class TesteFuncionarios {
+
+    public static void main(String[] args) {
+
+        Funcionario ana = new Funcionario(
+                "Ana Costa",
+                "F001",
+                2500,
+                "01/03/2020"
+        );
+
+        FuncionarioCLT bruno = new FuncionarioCLT(
+                "Bruno Reis",
+                "C001",
+                3000,
+                "10/06/2021",
+                220,
+                400
+        );
+
+        Gerente carla = new Gerente(
+                "Carla Mendes",
+                "G001",
+                8000,
+                "15/01/2018",
+                300,
+                500,
+                12,
+                0.20
+        );
+
+        Estagiario diego = new Estagiario(
+                "Diego Alves",
+                "E001",
+                0,
+                "05/08/2024",
+                1500,
+                20
+        );
+
+        System.out.println(ana);
+        System.out.println(bruno);
+        System.out.println(carla);
+        System.out.println(diego);
+    }
 }

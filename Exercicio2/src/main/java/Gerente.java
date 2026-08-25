c6d08cb8b25829a417641ea47f20e12c4d98662e
ytalo356476 <ninhar339@gmail.com>
2026-08-24 21:09:26 -0300

    Update Gerente.java

--- Exercicio2/src/main/java/Gerente.java
+++ Exercicio2/src/main/java/Gerente.java
@@ -1,3 +1,48 @@
-public class Gerente
-{
+public class Gerente extends FuncionarioCLT {
+
+    private int tamanhoEquipe;
+    private double percentualBonus;
+
+    public Gerente(
+            String nome,
+            String matricula,
+            double salarioBase,
+            String dataAdmissao,
+            double valeTransporte,
+            double valeAlimentacao,
+            int tamanhoEquipe,
+            double percentualBonus) {
+
+        super(
+                nome,
+                matricula,
+                salarioBase,
+                dataAdmissao,
+                valeTransporte,
+                valeAlimentacao
+        );
+
+        this.tamanhoEquipe = tamanhoEquipe;
+        this.percentualBonus = percentualBonus;
+    }
+
+    @Override
+    public double calcularSalario() {
+
+        double bonus = getSalarioBase() * percentualBonus;
+
+        return super.calcularSalario() + bonus;
+    }
+
+    @Override
+    public double calcularDesconto() {
+
+        double desconto = super.calcularDesconto();
+
+        if (tamanhoEquipe > 10) {
+            desconto += 100.00;
+        }
+
+        return desconto;
+    }
 }

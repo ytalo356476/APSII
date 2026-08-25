a2b984e1f3184e7b57f958b0b3c19f7d989754b6
ytalo356476 <ninhar339@gmail.com>
2026-08-24 21:07:22 -0300

    Update Estagiario.java

--- Exercicio2/src/main/java/Estagiario.java
+++ Exercicio2/src/main/java/Estagiario.java
@@ -1,2 +1,29 @@
-public class Estagiario {
+public class Estagiario extends Funcionario {
+
+    private double valorBolsaAuxilio;
+    private int cargaHorariaSemanal;
+
+    public Estagiario(
+            String nome,
+            String matricula,
+            double salarioBase,
+            String dataAdmissao,
+            double valorBolsaAuxilio,
+            int cargaHorariaSemanal) {
+
+        super(nome, matricula, salarioBase, dataAdmissao);
+
+        this.valorBolsaAuxilio = valorBolsaAuxilio;
+        this.cargaHorariaSemanal = cargaHorariaSemanal;
+    }
+
+    @Override
+    public double calcularSalario() {
+        return valorBolsaAuxilio;
+    }
+
+    @Override
+    public double calcularDesconto() {
+        return 0.00;
+    }
 }
